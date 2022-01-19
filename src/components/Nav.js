02d2100a3804558ce2d4229745a3d00713a6de3b
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-19 08:24:10 -0600

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+        <ul>
+            <li>about</li>
+            <li>collections</li>
+            <li>delivery service</li>
+            <li>location</li>
+            <li>contact</li>
+        </ul>
+    );
+}
+
+export default Nav;
